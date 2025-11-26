a69bff62ae364d69b93d2f4d0d974ac74a1a4dba
Matheus Kamer <matheus_kamer@hotmail.com>
2025-11-25 21:21:01 -0400

    feat: add @nestjs/mapped-types dependency and import CategoriesModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './modules/players/players.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PlayersModule } from './modules/players/players.module';
     }),
     MongooseModule.forRoot(process.env.DATABASE_URL!),
     PlayersModule,
+    CategoriesModule,
   ],
   controllers: [],
   providers: [],
