71184962958ccf8728318b538ff7cac897412802
Matheus Kamer <matheus_kamer@hotmail.com>
2025-11-25 21:21:12 -0400

    feat: implement Categories module with controller and service for category management, including player association

---
+++ src/modules/categories/categories.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoriesService.create(createCategoryDto);
+  }
+
+  @Post(':category/player/:playerId')
+  addPlayerToCategory(
+    @Param('category') category: string,
+    @Param('playerId') playerId: string,
+  ) {
+    return this.categoriesService.addPlayerToCategory(category, playerId);
+  }
+
+  @Get()
+  findAllCategories() {
+    return this.categoriesService.findAllCategories().populate('players');
+  }
+
+  @Get(':category')
+  findCategoryByName(@Param('category') category: string) {
+    return this.categoriesService.findCategoryByName(category);
+  }
+
+  @Patch(':category')
+  update(
+    @Param('category') category: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(category, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.categoriesService.remove(id);
+  }
+}
---
+++ src/modules/categories/categories.service.ts
@@ -0,0 +1,120 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CategoryDocument } from './schema/category.schema';
+import { PlayersService } from '../players/players.service';
+
+@Injectable()
+export class CategoriesService {
+  private readonly logger = new Logger(CategoriesService.name);
+
+  constructor(
+    @InjectModel('Category')
+    private readonly categoryModel: Model<CategoryDocument>,
+    private readonly playersService: PlayersService,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const { category } = createCategoryDto;
+
+    const categoryAlreadyExists = await this.categoryModel.findOne({
+      category,
+    });
+
+    if (categoryAlreadyExists) {
+      throw new BadRequestException('Category already exists!');
+    }
+
+    const createdCategory = await this.categoryModel.create(createCategoryDto);
+
+    this.logger.log(`createdCategory: ${JSON.stringify(createdCategory)}`);
+
+    return createdCategory;
+  }
+
+  async addPlayerToCategory(categoryName: string, playerId: string) {
+    const category = await this.categoryModel.findOne({
+      category: categoryName,
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not exists in DB!');
+    }
+
+    const player = await this.playersService.findPlayerById(playerId);
+
+    if (!player) {
+      throw new NotFoundException('Player not exists in DB!');
+    }
+
+    const playerAlreadyInCategory = category.players.some(
+      (p) => p._id.toString() === playerId,
+    );
+
+    if (playerAlreadyInCategory) {
+      throw new BadRequestException('Player already in this category!');
+    }
+
+    category.players.push(playerId as any);
+
+    return await this.categoryModel.findOneAndUpdate(
+      {
+        category: categoryName,
+      },
+      { $set: category },
+      { new: true },
+    );
+  }
+
+  findAllCategories() {
+    return this.categoryModel.find();
+  }
+
+  async findCategoryByName(categoryName: string) {
+    const category = await this.categoryModel.findOne({
+      category: categoryName,
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not exists in DB!');
+    }
+
+    this.logger.log(`Category fetched: ${JSON.stringify(category)}`);
+
+    return category;
+  }
+
+  async update(categoryName: string, updateCategoryDto: UpdateCategoryDto) {
+    const updatedCategory = await this.categoryModel.findOneAndUpdate(
+      { category: categoryName },
+      { $set: updateCategoryDto },
+      { new: true },
+    );
+
+    if (!updatedCategory) {
+      throw new NotFoundException('Category not exists in DB!');
+    }
+
+    return updatedCategory;
+  }
+
+  async remove(id: string) {
+    const deletedCategory = await this.categoryModel.findOneAndDelete({
+      _id: id,
+    });
+
+    if (!deletedCategory) {
+      throw new NotFoundException('Category not exists in DB!');
+    }
+
+    return deletedCategory;
+  }
+}
